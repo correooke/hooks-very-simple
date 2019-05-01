fab9a415781e83b3c9387526ef075f9a14b6065a
HSNBDES33\eocariz <eocariz@hasar.com>
2019-05-01 15:52:30 -0300

    Corrección archivo index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Hooks/App';
+import App from './NoHooks/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
